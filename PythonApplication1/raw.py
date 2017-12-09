dcbcc509cc2034a998695757a81fb7088299f057
atul bisht <atul26bi@gmail.com>
2017-12-10 01:22:40 +0530

    commented few lines

--- PythonApplication1/raw.py
+++ PythonApplication1/raw.py
@@ -9,13 +9,13 @@ def getjson(title,artist_name):
     YOUR_API_KEY= "9cfa2cee18f204c68b67fe0a4ce3b383"
     try:
         url= "http://ws.audioscrobbler.com/2.0/?method=track.search&track="+track+"&artist="+artist+"&limit=9&api_key="+YOUR_API_KEY+"&format=json"
-        print(url)
+        #print(url)
         json_r = urllib.request.urlopen(url).read()
         d = json.loads(json_r)
         with open('recived.json',"w") as out:
             json.dump(d,out)
         t= len(d["results"]["trackmatches"]["track"][0]["image"])
-        print(t)
+        #print(t)
         return d["results"]["trackmatches"]["track"][0]["name"],d["results"]["trackmatches"]["track"][0]["artist"],d["results"]["trackmatches"]["track"][0]["image"][t-1]["#text"]
     except :
         return "EXCEPTION RAISED"
